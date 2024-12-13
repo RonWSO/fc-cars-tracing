439276f92e30e4191fdf4d9ce16f870cfdc11d99
root <root@DESKTOP-E7K35AT>
2024-12-13 10:35:42 -0300

    aula 3, mas a rota dinamica não está sendo localizada, então tive que habilitar o cors pra todos

--- next-frontend/src/app/admin/page.tsx
+++ next-frontend/src/app/admin/page.tsx
@@ -1,13 +1,69 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useMap } from "../../hooks/useMap";
+import { socket } from "../../utils/socket-io";
+import { RouteModel } from "../../utils/models";
+
+export async function getRoute(route_id: string): Promise<RouteModel> {
+  const response = await fetch(`http://localhost:3000/routes/${route_id}`, {
+    cache: "force-cache",
+    next: {
+      tags: [`routes-${route_id}`, "routes"],
+    },
+  });
+  return response.json();
+}
 
 export function AdminPage() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  useMap(mapContainerRef);
+  const map = useMap(mapContainerRef);
 
-  return <div className="h-full w-full" ref={mapContainerRef} />;
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    socket.disconnected ? socket.connect() : socket.offAny();
+
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+
+    
+
+    socket.on(
+      `server:new-points:list`,
+      async (data: { route_id: string; lat: number; lng: number }) => {
+        console.log(data);
+        if (!map.hasRoute(data.route_id)) {
+          
+          const route = await getRoute(data.route_id);
+          //const route = await response.json();
+          map.addRouteWithIcons({
+            routeId: data.route_id,
+            startMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location
+            },
+            endMarkerOptions: {
+              position: route.directions.routes[0].legs[0].end_location
+            },
+            carMarkerOptions: {
+              position: route.directions.routes[0].legs[0].start_location
+            },
+          });
+        }
+        map.moveCar(data.route_id, { lat: data.lat, lng: data.lng });
+      }
+    );
+    return () => {
+      socket.disconnect();
+    }
+  }, [map]);
+
+
+  return <div style={{width: "100vw", height: "100vh"}} ref={mapContainerRef} />;
 }
 
 export default AdminPage;
